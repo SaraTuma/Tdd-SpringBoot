41c415c652a344535bc9f28587a5fd419ebf78f8
SaraTuma <saradavidtuma07@gmail.com>
2024-03-10 09:43:57 +0000

    Classe usuario

--- src/main/java/ao/com/tddspring/api/domain/User.java
+++ src/main/java/ao/com/tddspring/api/domain/User.java
@@ -1,6 +1,19 @@
 package ao.com.tddspring.api.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class User {
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
 }
